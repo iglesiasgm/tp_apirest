7a951d8bee0a5bb803e5a8c78401a8c94dc2ce8f
iglesiasgm <giglesias035@gmail.com>
2024-09-21 17:57:29 -0300

    first commit

---
+++ restinicial-main/src/main/java/com/example/inicial1/entities/Persona.java
@@ -0,0 +1,30 @@
+package com.example.inicial1.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.envers.Audited;
+
+import java.io.Serializable;
+
+@Entity
+@Table (name = "persona")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class Persona implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="nombre")
+    private String nombre;
+    @Column(name="apellido")
+    private String apellido;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_domicilio")
+    private Domicilio domicilio;
+}
+
